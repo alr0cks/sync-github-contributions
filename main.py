f66d8992a8af10044093d4376dc9cec686ef6c36
Alok Bhawankar <alok.bhawankar@gmail.com>
2023-03-05 03:04:57 +0530

    generate bash script to add git commit history

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from subprocess import run
 
 def get_contributions_data_per_day(username: str, year: int):
     url = f"https://github.com/users/{username}/contributions?tab=overview&from={year}-01-01&to={year}-12-31"
@@ -20,10 +21,29 @@ def get_contributions_data_per_day(username: str, year: int):
     
     return daily_contributions_list, total_contributions
 
+def generate_bash_script_linux_macos(contributions_list: list):
+    bash_script = open('script.sh', 'w')
+    for each_day_data in contributions_list:
+        script = f"""GIT_AUTHOR_DATE={each_day_data['date']}T12:00:00 GIT_COMMITER_DATE={each_day_data['date']}T12:00:00 git commit --allow-empty -m "Rewriting History!" > /dev/null\n"""
+        for each_contribution in range(each_day_data['contributions']):
+            bash_script.write(script)
+    bash_script.write("git pull origin main\n") 
+    bash_script.write("git push -f origin main")
+    bash_script.close()
+
 
 if __name__ == "__main__":
 
     username = str(input("Enter Github username for which you'd like to sync contributions: "))
     year = int(input("Enter year for which you'd like to sync contributions: "))
+
     contributions_list, total_contributions = get_contributions_data_per_day(username=username, year=year)
+    print(f"Total Contribution to be synced: {total_contributions}")
+
+    generate_bash_script_linux_macos(contributions_list=contributions_list)
+    print("Script successfully generated!!!!")
+
+    is_execute = input("Do you want to execute the generated script ? (Y/n): ")
+    if is_execute == 'Y' or is_execute == 'y':
+        run(['sh', 'script.sh'])
 
